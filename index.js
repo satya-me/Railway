b0c94cde3feea058387056e8b609e09df38b06d1
Satyajit Barik <satyajitbarik83@gmail.com>
2023-02-06 00:10:32 +0530

    ..

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require("node:http");
 
-const hostname = "127.0.0.1";
+const hostname = "https://railway-production-421d.up.railway.app";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
